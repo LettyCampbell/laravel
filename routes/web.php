fa2e49cc57443a439202367118630c26c4654ea0
LettyCampbell <leticiacampbellgoncalves@gmail.com>
2025-03-14 18:25:04 -0300

    implementação das rotas

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,7 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.principal')->middleware(LogAcessoMiddleware::class);
-
-Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
-Route::post('/contato', [App\Http\Controllers\ContatoController::class, 'salvar'])->name('site.contato');
-
-Route::get('/sobrenos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
-
-Route::get('/login/{erro?}', [App\Http\Controllers\LoginController::class, 'login'])->name('site.login');
-Route::post('/login', [App\Http\Controllers\LoginController::class, 'autenticar'])->name('site.login');
-
-Route::prefix('app')->group(function(){
-    
-    Route::get('/fornecedor', [App\Http\Controllers\FornecedoresController::class, 'index'])->name('app.fornecedor');
-    Route::get('/fornecedor/listar', [App\Http\Controllers\FornecedoresController::class, 'listar'])->name('app.fornecedor.listar');
-    Route::post('/fornecedor/listar', [App\Http\Controllers\FornecedoresController::class, 'listar'])->name('app.fornecedor.listar');
-    Route::get('/fornecedor/adicionar', [App\Http\Controllers\FornecedoresController::class, 'adicionar'])->name('app.fornecedor.adicionar');
-    Route::post('/fornecedor/adicionar', [App\Http\Controllers\FornecedoresController::class, 'adicionar'])->name('app.fornecedor.adicionar');
-    Route::get('/fornecedor/editar/{id}', [App\Http\Controllers\FornecedoresController::class, 'editar'])->name('app.fornecedor.editar');
-    
-    Route::get('/clientes', [App\Http\Controllers\ClientesController::class, 'clientes'])->name('app.clientes');
-    Route::get('/produtos', [App\Http\Controllers\ProdutosController::class, 'produtos'])->name('app.produtos');
-});
-
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/registrar', [App\Http\Controllers\Registrar::class, 'registrar']);
+Route::get('/adocao', [App\Http\Controllers\Adocao::class, 'adocao']);
+Route::get('/cadastro-animal', [App\Http\Controllers\CadastroAnimal::class, 'cadastroAnimal']);
